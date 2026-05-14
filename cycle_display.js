5655b4757f8495174a75b36ad51823d9fa106f4f
Taylor Daniels <tdan1101@gmail.com>
2026-05-14 19:03:43 -0400

    may26update

---
+++ cycle_display.js
@@ -0,0 +1,19 @@
+<script type = "text/javascript">
+          function displayNextImage() {
+              x = (x === images.length - 1) ? 0 : x + 1;
+              document.getElementById("img").src = images[x];
+          }
+		
+          function displayPreviousImage() {
+              x = (x <= 0) ? images.length - 1 : x - 1;
+              document.getElementById("img").src = images[x];
+          }
+		
+          function startTimer() {
+              setInterval(displayNextImage, 10000);
+          }
+
+          var images = [], x = -1;
+          images[0] = "Taylorportrait.jpg";
+          images[1] = "fractal.jpg";
+          </script>
